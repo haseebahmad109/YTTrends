88b76aea9d004876d92c3255fbaf7cb9f12705f3
Haseeb Ahmad <haseebahmad109@gmail.com>
2016-10-30 22:34:16 +0500

    rails project init commit

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,20 @@
+class ApiController < ApplicationController
+  
+  GOOGLE_API_KEY = "AIzaSyAW-MFxkJIJonEuF5E9ZP77c2y8hIeDlqg"
+
+  def videos
+    query = params.slice(:part, :regionCode, :id, :hl, :pageToken, :chart, :maxResults, :videoCategoryId)
+    query[:key] = GOOGLE_API_KEY
+    res = HTTParty.get("https://www.googleapis.com/youtube/v3/videoCategories", query: query)
+
+    render json: res.body
+  end
+
+  def video_categories
+    query = params.slice(:part, :regionCode, :id, :hl, :pageToken)
+    query[:key] = GOOGLE_API_KEY
+    res = HTTParty.get("https://www.googleapis.com/youtube/v3/videoCategories", query: query)
+
+    render json: res.body
+  end
+end
