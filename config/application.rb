3777e477903194fb1c0ff13a7a4b52c6c5a704af
AWS server GENERAL <haseebahmad109@gmail.com>
2016-10-31 14:14:08 +0000

    deployed yttrends.comwosolutions.com

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module YTTrends
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end  
+    end
+
   end
 end
